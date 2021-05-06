283a9b862430c61f2fea7cc8ad9d5989ebcf0af6
Behnoosh <behnoosh.beheshti@gmail.com>
2021-05-06 20:12:28 +0200

    Create React dictionary App file with footer

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.png";
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="logo img-fluid" alt="logo" />
+
+      <footer>
+        Open-sourced on{" "}
+        <a
+          href="https://github.com/behnooshbeheshti/react-dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          Netlify
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://www.linkedin.com/in/behnoosh-beheshti/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Behnoosh Beheshti
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
